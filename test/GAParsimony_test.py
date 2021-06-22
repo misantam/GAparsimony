3e92569e992816e09e706575258f73634488978f
Millan <misantam1998@gmail.com>
2021-06-22 19:50:22 +0200

    Actualizados tests

--- test/GAParsimony_test.py
+++ test/GAParsimony_test.py
@@ -30,24 +30,24 @@ def test_GAParsimony_regression_boston_population(population):
     assert (pop.population==np.array(population["population_resultado"])).all()
 
 
-data = readJSONFile('./test/outputs/populationClass.json')
-population = Population(data["params"], data["features"], np.array(data["population"]))
+# data = readJSONFile('./test/outputs/populationClass.json')
+# population = Population(data["params"], data["features"], np.array(data["population"]))
 
-@pytest.mark.parametrize("population, slice, value, resultado", 
-                        [(population,(slice(2), slice(None)), np.arange(20), np.array(data["population_1"], dtype=object)),
-                        (population,(slice(2), slice(None)), np.array([np.arange(20), np.arange(1, 21)]), np.array(data["population_2"], dtype=object)),
-                        (population,(slice(2), slice(None)), 0, np.array(data["population_3"], dtype=object)),
-                        (population,(1, slice(2)), 1, np.array(data["population_4"], dtype=object)),
-                        (population,(1, slice(None)), np.arange(20), np.array(data["population_5"], dtype=object)),
-                        (population,(1, slice(2)), np.array([2,2]), np.array(data["population_6"], dtype=object)),
-                        (population,(slice(None), 2), 1, np.array(data["population_7"], dtype=object)),
-                        (population,(slice(None), 6), 87, np.array(data["population_8"], dtype=object)),
-                        (population,(slice(None), 7), 98, np.array(data["population_9"], dtype=object))])
-def test_GAParsimony_regression_population_class(population, slice, value, resultado):
+# @pytest.mark.parametrize("population, slice, value, resultado", 
+#                         [(population,(slice(2), slice(None)), np.arange(20), np.array(data["population_1"], dtype=object)),
+#                         (population,(slice(2), slice(None)), np.array([np.arange(20), np.arange(1, 21)]), np.array(data["population_2"], dtype=object)),
+#                         (population,(slice(2), slice(None)), 0, np.array(data["population_3"], dtype=object)),
+#                         (population,(1, slice(2)), 1, np.array(data["population_4"], dtype=object)),
+#                         (population,(1, slice(None)), np.arange(20), np.array(data["population_5"], dtype=object)),
+#                         (population,(1, slice(2)), np.array([2,2]), np.array(data["population_6"], dtype=object)),
+#                         (population,(slice(None), 2), 1, np.array(data["population_7"], dtype=object)),
+#                         (population,(slice(None), 6), 87, np.array(data["population_8"], dtype=object)),
+#                         (population,(slice(None), 7), 98, np.array(data["population_9"], dtype=object))])
+# def test_GAParsimony_regression_population_class(population, slice, value, resultado):
 
-    population[slice] = value
+#     population[slice] = value
     
-    assert (population.population==resultado).all()
+#     assert (population.population==resultado).all()
 
 #################################################
 #*****************TEST RERANK*******************#
