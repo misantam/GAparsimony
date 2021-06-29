3649b3db62049323b93a0cf6cc023ebf19d3cf16
Javier Martínez de Pisón <fjmartin@unirioja.es>
2021-06-29 21:29:08 +0200

    Changed model to algorithm, regression to maximize.
    Cambiados los parámetros y eliminado el cv=None

--- GAparsimony/util/fitness.py
+++ GAparsimony/util/fitness.py
@@ -4,23 +4,23 @@ from sklearn.model_selection import train_test_split, cross_val_score, RepeatedK
 from sklearn.metrics import make_scorer
 import numpy as np
 
-def getFitness(model, metric, complexity, cv=None, regression=True, test_size=0.2, random_state=42, n_jobs=-1):
+def getFitness(algorithm, metric, complexity, cv=RepeatedKFold(n_splits=10, n_repeats=5, random_state=42), maximize=True, test_size=0.2, random_state=42, n_jobs=-1):
     r"""
-    Generating function of a fitness function.
+     Fitness function for GAparsimony.
 
     Parameters
     ----------
-    model : object
-        The model we want to optimize. 
+    algorithm : object
+        The machine learning function to optimize. 
     metric : function
-        A function that computes the metric.
+        A function that computes the fitness value.
     complexity : function
-        A function that calculates the complexity of the model. You can use for some models those defined in `GAparsimony.util.complexity`
+        A function that calculates the complexity of the model. There are some functions available in `GAparsimony.util.complexity`.
     cv : object, optional
-        An `sklearn.model_selection`, Splitter Classes. If `None`, `sklearn.model_selection.RepeatedKFold`. Default `None`
-    regression : bool, optional
-        If it is a regression model `True` else `False`.
-    test_size : float, optional
+        An `sklearn.model_selection` function. By default, is defined `RepeatedKFold(n_splits=10, n_repeats=5, random_state=42)`.
+    maximize : bool, optional
+        `True`, if the objective is to maximize the metric, to minimize it, set to `False`.
+    test_size : float, int, None
         If float, should be between 0.0 and 1.0 and represent the proportion
         of the dataset to include in the test split. If int, represents the
         absolute number of test samples. If None, the value is set to the
@@ -44,19 +44,17 @@ def getFitness(model, metric, complexity, cv=None, regression=True, test_size=0.
         from sklearn.metrics import cohen_kappa_score
 
         from GAparsimony import getFitness
-        from GAparsimony.util import svm
+        from GAparsimony.util import svm_complexity
 
-        fitness = getFitness(SVC, cohen_kappa_score, svm, cv, regression=False, test_size=0.2, random_state=42, n_jobs=-1)
+        fitness = getFitness(SVC, cohen_kappa_score, svm_complexity, cv, maximize=True, test_size=0.2, random_state=42, n_jobs=-1)
     """
 
-    if model is None:
-        raise Exception("A model class must be provided!!!")
+    if algorithm is None:
+        raise Exception("An algorithm function must be provided!!!")
     if metric is None or not callable(metric):
         raise Exception("A metric function must be provided!!!")
     if complexity is None or not callable(complexity):
         raise Exception("A complexity function must be provided!!!")
-    if cv is None:
-        cv = RepeatedKFold(n_splits=10, n_repeats=10, random_state=random_state)
     
     def fitness(cromosoma, **kwargs):
         if "pandas" in str(type(kwargs["X"])):
@@ -71,12 +69,12 @@ def getFitness(model, metric, complexity, cv=None, regression=True, test_size=0.
 
             # train the model
 
-            aux = model(**cromosoma.params)
+            aux = algorithm(**cromosoma.params)
             fitness_val = cross_val_score(aux, data_train_model, y_train, scoring=make_scorer(metric), cv=cv, n_jobs=n_jobs).mean()
-            modelo = model(**cromosoma.params).fit(data_train_model, y_train)
+            modelo = algorithm(**cromosoma.params).fit(data_train_model, y_train)
             fitness_test = metric(modelo.predict(data_test_model), y_test)
 
-            if regression:
+            if maximize:
                 fitness_val = -fitness_val
                 fitness_test = -fitness_test
 
